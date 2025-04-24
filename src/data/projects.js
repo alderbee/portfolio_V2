50ddeeecaeff638b902c9be215cb5b359f7333bf
asvajrashetti <asvajrashetti@gmail.com>
2025-04-23 19:36:49 -0700

    Fix image path route

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,3 +1,8 @@
+import portfolioImg from "../assets/projects/portfolio.png";
+import echochatImg from "../assets/projects/echochat.png";
+import taskhubImg from "../assets/projects/taskhub.png";
+import safestoreImg from "../assets/projects/safestore.png";
+
 export const projects = [
   {
     name: "Portfolio(React)",
@@ -5,7 +10,7 @@ export const projects = [
       "Constructed with React, Tailwind CSS, and a mobile-first approach. Configured Cloudflare and deployed with Git Actions to Netlify.",
     tech: ["react", "tailwind", "material", "netlify"],
     link: "https://amitsv.com/",
-    image: "src/assets/projects/portfolio.png",
+    image: portfolioImg,
   },
   {
     name: "EchoChat",
@@ -13,7 +18,7 @@ export const projects = [
       "EchoChat is a lightweight social room chat system. Users can enter a username, create a room, and start chatting in real time implmented using websocket. It's built using React, ASP.NET Core 8, and SignalR, and is perfect for learning or building on top of real-time technologies.",
     tech: ["dotnet", "react"],
     link: "https://github.com/alderbee/EchoChat",
-    image: "src/assets/projects/echochat.png",
+    image: echochatImg,
   },
   {
     name: "TaskHub",
@@ -21,13 +26,13 @@ export const projects = [
       "Task Hub is a full-stack web application designed to help users efficiently manage personal tasks: follows the CQRS pattern with MediatR and Clean Architecture principles for better separation of concerns and maintainability.",
     tech: ["jwt", "oauth"],
     link: "https://github.com/alderbee/task-hub",
-    image: "src/assets/projects/taskhub.png",
+    image: taskhubImg,
   },
   {
     name: "SafeStore",
     description:
       "Secure file upload API using AWS EC2, S3 for storage, SMTP for validation, and IAM access control.",
     tech: ["aws", "python"],
-    image: "src/assets/projects/safestore.png",
+    image: safestoreImg,
   },
 ];
